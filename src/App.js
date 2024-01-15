8819ee4875fb8de6526c72af6bccb933795e4a68
Jhojan Tobón <jhojantobonm@gmail.com>
2024-01-15 11:27:13 -0500

    Created searcher component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Col } from "antd";
 import "./App.css";
+import { Searcher } from "./components/Searcher";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Searcher.jsx
@@ -0,0 +1,6 @@
+import { Input } from "antd";
+import React from "react";
+
+export const Searcher = () => {
+  return <Input.Search placeholder="Search..."></Input.Search>;
+};
