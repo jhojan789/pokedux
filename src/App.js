5ec371723aa608c8ebe93914b1886faaa0737acd
Jhojan Tobón <jhojantobonm@gmail.com>
2024-01-15 17:15:22 -0500

    Created and added fetch function.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Col } from "antd";
-import "./App.css";
 import { Searcher } from "./components/Searcher";
 import { PokemonList } from "./components/PokemonList";
 import logo from "./static/logo.svg";
+import { fetchPokemons } from "./api/fetchPokemons";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    (async () => {
+      setPokemons(await fetchPokemons());
+    })();
+  }, []);
   return (
     <div className="App">
       <Col span={4} offset={10}>
@@ -13,7 +21,7 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList />
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { PokemonCard } from "./PokemonCard";
 import "./PokemonList.css";
 
 export const PokemonList = ({ pokemons }) => {
   return (
     <div className="PokemonList">
-      {pokemons.map((p, i) => (
-        <PokemonCard key={i} />
+      {pokemons.map((pokemon, i) => (
+        <PokemonCard key={i} name={pokemon.name} />
       ))}
     </div>
   );
