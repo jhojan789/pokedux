da6a5fba2b44b2927830ac54e7099b3078a5c9fc
Jhojan Tobón <jhojantobonm@gmail.com>
2024-01-16 15:32:32 -0500

    Added middleware.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
 import { pokemonReducer } from "./reducer/pokemonReducer";
+import { featuring, logger } from "./middleware";
 
-const store = createStore(
-  pokemonReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(featuring, logger)
 );
+
+const store = createStore(pokemonReducer, composedEnhancers);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
