da47701bf0c6c334e9443c19a389e9c380c35293
Jhojan Tobón <jhojantobonm@gmail.com>
2024-01-18 18:49:47 -0500

    Testing mutable and immutable.

---
+++ src/imnmutable.js
@@ -0,0 +1,28 @@
+//mutable
+// function updateHeight(userInf) {
+//   userInf.height = userInf.height + 1;
+//   return userInf;
+// }
+//
+//immutable with Object.assign
+// function updateHeight(userInf) {
+//   return Object.assign({}, userInf, { height: userInf.height + 1 });
+// }
+//
+//immutable with Object.assign
+function updateHeight(userInf) {
+  return { ...userInf, height: userInf.height + 1 };
+}
+
+const userInfo = {
+  name: "Jhojan",
+  height: 187,
+};
+
+console.log("userInfo BEFORE: ", userInfo);
+
+const updatedUserInfo = updateHeight(userInfo);
+
+console.log("userInfo AFTER: ", userInfo);
+
+console.log("updatedUserInfo", updatedUserInfo);
